3899213a2c2e39b1c2f85b2c60f410196c93eb3e
David <david.20797@gmail.com>
2026-01-01 15:07:38 +0200

    handing binomial long numbers and strings

--- Binomial.java
+++ Binomial.java
@@ -7,7 +7,8 @@ public class Binomial {
     	// System.out.println(binomial1(Integer.parseInt(args[0]), Integer.parseInt(args[1])));
 
 		// Testing the optimized binomial implementation:
-		// System.out.println(binomial(Integer.parseInt(args[0]), Integer.parseInt(args[1])));
+		// System.out.println(binomial(50, 25));
+		// System.out.println(binomial(50, 25).getClass());
 	}
 
 	// Computes the Binomial function, basic version.
@@ -17,42 +18,52 @@ public class Binomial {
 		return binomial1(n - 1, k) + binomial1(n - 1, k - 1);
 	}
 	
-	// Computes the Binomial function, efficiently
-	public static int binomial(int n, int k) {
-		// Invalid cases
-		if (k < 0 || k > n) return 0;
+	
+	private static long[][] emptyArr(int rows , int cols){
+		
+		long[][] a = new long[rows][cols];
 
+		for (int i = 0; i < a.length; i++) {
+			for (int j = 0; j < a[i].length; j++) {
+				a[i][j] = -1;
+			}
+		}
+		return a;
+	}
+
+	// Computes the Binomial function, efficiently
+	public static String binomial(int n, int k) {
+		// Invalid cases
+		if (k < 0 || k > n) return "0";
+		if (k == 0 || n == 0) return "1";
 		// Symmetry: C(n, k) = C(n, n-k)
 		if (k > n - k) {
 			k = n - k;
 		}
 
-		int[][] memo = new int[n + 1][k + 1];
-		for (int i = 0; i < memo.length; i++) {
-			for (int j = 0; j < memo[i].length; j++) {
-				memo[i][j] = -1;
-			}
-		}
+		long[][] memo = emptyArr(n+1, k+1);
+
 		return binomial(n, k, memo);
 	}
 
-	private static int binomial(int n, int k, int[][] memo) {
+
+	private static String binomial(int n, int k, long[][] memo) {
 		// Guard against bad indices
-		if (k < 0 || k > n) return 0;
+		if (k < 0 || k > n) return "0";
 
 		// Base cases
 		if (k == 0 || k == n) {
-			return 1;
+			return "1";
 		}
 
 		// Already computed?
 		if (memo[n][k] != -1) {
-			return memo[n][k];
+			return ""+memo[n][k];
 		}
 
 		// Recurrence with memoization
-		memo[n][k] = binomial(n - 1, k, memo) + binomial(n - 1, k - 1, memo);
-		return memo[n][k];
+		memo[n][k] = Long.parseLong(binomial(n - 1, k, memo)) + Long.parseLong(binomial(n - 1, k - 1, memo));
+		return ""+memo[n][k];
 	}
 
 }
